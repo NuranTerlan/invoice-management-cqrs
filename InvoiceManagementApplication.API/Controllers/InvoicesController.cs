11558bc5a34db25928d9e8022653487026e5a57e
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:07:14 +0400

    Create InvoicesController.cs

---
+++ InvoiceManagementApplication.API/Controllers/InvoicesController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using InvoiceManagementApplication.Application.Common.Interfaces;
+using InvoiceManagementApplication.Application.Invoices.Commands;
+using InvoiceManagementApplication.Application.Invoices.DTOs;
+using InvoiceManagementApplication.Application.Invoices.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceManagementApplication.API.Controllers
+{
+    [Authorize]
+    public class InvoicesController : ApiController
+    {
+        private readonly ICurrentUserService _currentUserService;
+
+        public InvoicesController(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+
+        [HttpPost("/api/invoices")]
+        public async Task<int> Create([FromBody] CreateInvoiceCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+
+        [HttpGet("api/invoices")]
+        public async Task<IList<InvoiceDto>> Get()
+        {
+            var query = new GetUserInvoicesQuery { UserId = _currentUserService.UserId };
+            return await Mediator.Send(query);
+        }
+    }
+}
