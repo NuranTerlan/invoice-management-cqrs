5445450689c29dd1b7d5bf8a5572475cb5d2bbe2
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:08:28 +0400

    Create CreateInvoiceCommand.cs

---
+++ InvoiceManagementApplication.Application/Invoices/Commands/CreateInvoiceCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using InvoiceManagementApplication.Application.Invoices.DTOs;
+using InvoiceManagementApplication.Domain.Enums;
+using MediatR;
+
+namespace InvoiceManagementApplication.Application.Invoices.Commands
+{
+    public class CreateInvoiceCommand : IRequest<int>
+    {
+        public CreateInvoiceCommand()
+        {
+            InvoiceItems = new List<InvoiceItemDto>();
+        }
+
+        public string InvoiceNumber { get; set; }
+        public string Logo { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime Date { get; set; }
+        public string PaymentTerms { get; set; }
+        public DateTime? DueDate { get; set; }
+        public float Discount { get; set; }
+        public DiscountType DiscountType { get; set; }
+        public float Tax { get; set; }
+        public TaxType TaxType { get; set; }
+        public float AmountPaid { get; set; }
+        public IList<InvoiceItemDto> InvoiceItems { get; set; }
+    }
+}
