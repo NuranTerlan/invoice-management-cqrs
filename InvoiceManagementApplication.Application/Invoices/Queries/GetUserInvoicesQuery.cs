83b383b8f925b669cad596c12d5ab09cdf32f8b1
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:08:12 +0400

    Create GetUserInvoicesQuery.cs

---
+++ InvoiceManagementApplication.Application/Invoices/Queries/GetUserInvoicesQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using InvoiceManagementApplication.Application.Invoices.DTOs;
+using MediatR;
+
+namespace InvoiceManagementApplication.Application.Invoices.Queries
+{
+    public class GetUserInvoicesQuery : IRequest<IList<InvoiceDto>>
+    {
+        public string UserId { get; set; }
+    }
+}
