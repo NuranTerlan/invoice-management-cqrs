9b12a75bf1201ec382a6e14bc29488764b5b1a7f
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:07:46 +0400

    Create IApplicationDbContext.cs

---
+++ InvoiceManagementApplication.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using InvoiceManagementApplication.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvoiceManagementApplication.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Invoice> Invoices { get; set; }
+        DbSet<InvoiceItem> InvoiceItems { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
