d4188bcbca2e2acce7a473d80fc0ad818c99f3bf
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:07:11 +0400

    Create ApiController.cs

---
+++ InvoiceManagementApplication.API/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvoiceManagementApplication.API.Controllers
+{
+    public abstract class ApiController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();
+    }
+}
