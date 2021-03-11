0ac2d39539eae412bfbfada72f48607acc39b2ce
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:07:51 +0400

    Create DependencyInjection.cs

---
+++ InvoiceManagementApplication.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using InvoiceManagementApplication.Application.Common.Interfaces;
+using InvoiceManagementApplication.Infrastructure.Data;
+using InvoiceManagementApplication.Infrastructure.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvoiceManagementApplication.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    optionsBuilder => optionsBuilder
+                        .MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<IApplicationDbContext>(provider => 
+                provider.GetService<ApplicationDbContext>());
+
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+
+            return services;
+        }
+    }
+}
