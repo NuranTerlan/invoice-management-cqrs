f255630ee952cb586ac5430174a28bf0a2c55ead
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:08:01 +0400

    Create InvoiceItem.cs

---
+++ InvoiceManagementApplication.Domain/Entities/InvoiceItem.cs
@@ -0,0 +1,15 @@
+﻿using InvoiceManagementApplication.Domain.Common;
+
+namespace InvoiceManagementApplication.Domain.Entities
+{
+    public class InvoiceItem : AuditEntity
+    {
+        public int Id { get; set; }
+        public int InvoiceId { get; set; }
+        public string Item { get; set; }
+        public float Quantity { get; set; }
+        public float Rate { get; set; }
+
+        public Invoice Invoice { get; set; }
+    }
+}
