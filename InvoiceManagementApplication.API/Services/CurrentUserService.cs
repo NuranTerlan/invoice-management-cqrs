9a4fbf2f7c5f4d4b536cbe96e80b3d6218b619ca
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:07:37 +0400

    Create CurrentUserService.cs

---
+++ InvoiceManagementApplication.API/Services/CurrentUserService.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using InvoiceManagementApplication.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace InvoiceManagementApplication.API.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public string UserId { get; }
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            UserId = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
