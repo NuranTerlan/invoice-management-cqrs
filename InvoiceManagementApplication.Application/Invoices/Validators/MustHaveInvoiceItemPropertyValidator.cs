b38bd42c9630ad9c9bb09cec239f7e76a2c1b9d9
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:08:08 +0400

    Create MustHaveInvoiceItemPropertyValidator.cs

---
+++ InvoiceManagementApplication.Application/Invoices/Validators/MustHaveInvoiceItemPropertyValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FluentValidation.Validators;
+using InvoiceManagementApplication.Application.Invoices.DTOs;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace InvoiceManagementApplication.Application.Invoices.Validators
+{
+    public class MustHaveInvoiceItemPropertyValidator : PropertyValidator
+    {
+        public MustHaveInvoiceItemPropertyValidator()
+            :base("Property {PropertyName} should not be an empty list")
+        {
+            
+        }
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            var list = context.PropertyValue as IList<InvoiceItemDto>;
+            return list != null && list.Any();
+        }
+    }
+}
