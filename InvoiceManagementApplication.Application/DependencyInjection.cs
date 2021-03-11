e3c64fc39144e82ee00d88936fdff0b57bbf450d
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:08:30 +0400

    Create DependencyInjection.cs

---
+++ InvoiceManagementApplication.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using FluentValidation;
+using InvoiceManagementApplication.Application.Common.Behaviors;
+using InvoiceManagementApplication.Application.Invoices.Commands;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvoiceManagementApplication.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(CreateInvoiceCommand));
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
