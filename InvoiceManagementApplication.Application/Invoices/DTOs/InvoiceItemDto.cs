1cad275336ed3544f8feb852320381eba4651314
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:08:20 +0400

    Create InvoiceItemDto.cs

---
+++ InvoiceManagementApplication.Application/Invoices/DTOs/InvoiceItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace InvoiceManagementApplication.Application.Invoices.DTOs
+{
+    public class InvoiceItemDto
+    {
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public float Quantity { get; set; }
+        public float Rate { get; set; }
+
+        public float Amount => Rate * Quantity;
+    }
+}
