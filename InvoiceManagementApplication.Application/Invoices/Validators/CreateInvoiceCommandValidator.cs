89e993ad18f2a92e433569f88521881cb76c8583
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:08:10 +0400

    Create CreateInvoiceCommandValidator.cs

---
+++ InvoiceManagementApplication.Application/Invoices/Validators/CreateInvoiceCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using InvoiceManagementApplication.Application.Invoices.Commands;
+
+namespace InvoiceManagementApplication.Application.Invoices.Validators
+{
+    public class CreateInvoiceCommandValidator : AbstractValidator<CreateInvoiceCommand>
+    {
+        public CreateInvoiceCommandValidator()
+        {
+            RuleFor(i => i.AmountPaid).NotNull();
+            RuleFor(i => i.Date).NotNull();
+            RuleFor(i => i.From).NotEmpty().MinimumLength(3);
+            RuleFor(i => i.To).NotEmpty().MinimumLength(3);
+            RuleFor(i => i.InvoiceItems)
+                .SetValidator(new MustHaveInvoiceItemPropertyValidator());
+        }
+    }
+}
