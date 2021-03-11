6ba2bf8c9e35c611a7a5ab961f32876afb0e7cc3
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-11 22:08:05 +0400

    Create AuditEntity.cs

---
+++ InvoiceManagementApplication.Domain/Common/AuditEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InvoiceManagementApplication.Domain.Common
+{
+    public class AuditEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreationTime { get; set; }
+
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModificationTime { get; set; }
+    }
+}
