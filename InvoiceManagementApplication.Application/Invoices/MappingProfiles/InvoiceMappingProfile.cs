8f0bc86a7bc9ba16823146457eeeea56da5c91a9
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-12 21:45:11 +0400

    Create InvoiceMappingProfile.cs

---
+++ InvoiceManagementApplication.Application/Invoices/MappingProfiles/InvoiceMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using InvoiceManagementApplication.Application.Invoices.Commands;
+using InvoiceManagementApplication.Application.Invoices.DTOs;
+using InvoiceManagementApplication.Domain.Entities;
+
+namespace InvoiceManagementApplication.Application.Invoices.MappingProfiles
+{
+    public class InvoiceMappingProfile : Profile
+    {
+        public InvoiceMappingProfile()
+        {
+            CreateMap<Invoice, InvoiceDto>().ReverseMap();
+
+            // we can declare mapped properties by hard coding it like code below
+            CreateMap<InvoiceItem, InvoiceItemDto>().ConstructUsing(i => new InvoiceItemDto
+            {
+                Id = i.Id,
+                Item = i.Item,
+                Quantity = i.Quantity,
+                Rate = i.Rate
+            }).ReverseMap();
+
+            CreateMap<CreateInvoiceCommand, Invoice>();
+        }
+    }
+}
