8398072d91c12f539828ae1fe7f30d5b7b61b2b9
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-12 21:45:00 +0400

    hard-coded entity-dto relation is simplified by using automapper

--- InvoiceManagementApplication.Application/Invoices/Handlers/CreateInvoiceCommandHandler.cs
+++ InvoiceManagementApplication.Application/Invoices/Handlers/CreateInvoiceCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using InvoiceManagementApplication.Application.Common.Interfaces;
 using InvoiceManagementApplication.Application.Invoices.Commands;
 using InvoiceManagementApplication.Domain.Entities;
@@ -11,35 +12,17 @@ namespace InvoiceManagementApplication.Application.Invoices.Handlers
     public class CreateInvoiceCommandHandler : IRequestHandler<CreateInvoiceCommand, int>
     {
         private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public CreateInvoiceCommandHandler(IApplicationDbContext context)
+        public CreateInvoiceCommandHandler(IApplicationDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         public async Task<int> Handle(CreateInvoiceCommand request, CancellationToken cancellationToken)
         {
-            var invoice = new Invoice
-            {
-                AmountPaid = request.AmountPaid,
-                Date = request.Date,
-                Discount = request.Discount,
-                DiscountType = request.DiscountType,
-                Tax = request.Tax,
-                TaxType = request.TaxType,
-                DueDate = request.DueDate,
-                From = request.From,
-                To = request.To,
-                InvoiceNumber = request.InvoiceNumber,
-                Logo = request.Logo,
-                PaymentTerms = request.PaymentTerms,
-                InvoiceItems = request.InvoiceItems.Select(item => new InvoiceItem
-                {
-                    Item = item.Item,
-                    Quantity = item.Quantity,
-                    Rate = item.Rate
-                }).ToList()
-            };
+            var invoice = _mapper.Map<Invoice>(request);
 
             await _context.Invoices.AddAsync(invoice, cancellationToken);
             await _context.SaveChangesAsync(cancellationToken);
